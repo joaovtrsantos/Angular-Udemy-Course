03d24c8970f8460e833ba505de610527a820209a
joaovtrsantos <dev.joaovitor1@gmail.com>
2025-09-22 20:33:12 -0300

    feat: creating card button component

--- components-project/src/app/app-module.ts
+++ components-project/src/app/app-module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
 import { Card } from './card/card';
+import { CardButton } from './card-button/card-button';
 
 @NgModule({
   declarations: [
     App,
-    Card
+    Card,
+    CardButton
   ],
   imports: [
     BrowserModule,
